a0751726474390cf1669b241446e3ec4c3eb0633
DebyGrey <demifashoro@gmail.com>
2023-08-25 10:02:57 +0100

    Adds foreign key to user has many likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # Associations
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   # Methods
   def recent_posts
